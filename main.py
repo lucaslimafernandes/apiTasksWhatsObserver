7373ff6e58c769b2ce585d76e60c0e21c2141ab0
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2023-04-09 17:09:50 -0300

    proteção api

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from typing import Union, List
+from typing_extensions import Annotated
 import settings
 import requests
 from application.observer import Assunto, WPP
 from application.facade import ClientWeather
+from hashlib import sha256
 
 app = FastAPI()
 
@@ -30,16 +33,28 @@ async def sender(message:str) -> None:
 
 
 @app.get('/clima/')
-async def clima() -> None:
-    cw = ClientWeather()
-    res = cw.obter_clima()
+async def clima(x_token: Annotated[Union[List[str], None], Header()] = None) -> None:
+    try:
+        if settings.TK == sha256({'X-Token values': x_token[0]}['X-Token values'].encode()).hexdigest():
+        
+            cw = ClientWeather()
+            res = cw.obter_clima()
 
-    formatt = f'''Cidade: {res['cidade']}\nNuvens: {res['clouds']}\nTemperatura: {res['main']['temp']}\n\tSensação Térmica: {res['main']['feels_like']}\n\tMínima: {res['main']['temp_min']}\n\tMáxima: {res['main']['temp_max']}\n\tHumidade: {res['main']['humidity']}\nVento: {res['wind']}'''
-    
-    ins = Assunto()
-    WPP(ins)
-    ins.add_noticia(formatt)
-    ins.enviar()
-    
-    return res
+            formatt = f'''Cidade: {res['cidade']}\nNuvens: {res['clouds']}\nTemperatura: {res['main']['temp']}\n\tSensação Térmica: {res['main']['feels_like']}\n\tMínima: {res['main']['temp_min']}\n\tMáxima: {res['main']['temp_max']}\n\tHumidade: {res['main']['humidity']}\nVento: {res['wind']}'''
+            
+            ins = Assunto()
+            WPP(ins)
+            ins.add_noticia(formatt)
+            ins.enviar()
+        
+            return res
+    except:
+        return {'Error': 'Please, contact admnistrator!'}
 
+
+@app.get('/teste/')
+async def teste(x_token: Annotated[Union[List[str], None], Header()] = None):
+    
+    a = {"X-Token values": x_token[0]}
+    
+    return a['X-Token values']
--- settings_model.py
+++ settings_model.py
@@ -1,2 +1,10 @@
+
 API_KEY = 1234
-PHONE_NUMBER = 5551999999999
+PHONE_NUMBER = 5511999999999
+
+OPEN_WEATHER = 'TOKEN'
+LAT_LONG = {
+            'local': [0, 0]
+            }
+
+TK = 'headerrrr'
